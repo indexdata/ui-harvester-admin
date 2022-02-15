0e121edf5ed97aa41ce07e619e269f8f22f15b4a
Mike Taylor <mike@miketaylor.org.uk>
2022-02-15 16:29:18 +0000

    Abandon attempt to use hot reloading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { hot } from 'react-hot-loader';
 import { Switch } from 'react-router-dom';
 import { Route as NestedRoute } from '@folio/stripes/core';
 import Settings from './settings';
@@ -32,4 +31,4 @@ class HarvesterAdminApp extends React.Component {
   }
 }
 
-export default hot(module)(HarvesterAdminApp);
+export default HarvesterAdminApp;
