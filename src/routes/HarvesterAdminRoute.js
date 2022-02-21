6d3fb67026d225d93ff0a526aa6d0d8247de8917
Mike Taylor <mike@miketaylor.org.uk>
2022-02-21 19:06:46 +0000

    Use <SearchAndSortQuery> to display harvestables

--- src/routes/HarvesterAdminRoute.js
+++ src/routes/HarvesterAdminRoute.js
@@ -1,40 +1,93 @@
 import React, { useState } from 'react';
-import { useOkapiKy } from '@folio/stripes/core';
-import { LoadingPane } from '@folio/stripes/components';
-import ErrorMessage from '../util/ErrorMessage';
+import PropTypes from 'prop-types';
+import { stripesConnect } from '@folio/stripes/core';
+import { makeQueryFunction, StripesConnectedSource } from '@folio/stripes/smart-components';
+import HarvesterAdmin from '../views/HarvesterAdmin';
 
-const HarvesterAdminRoute = () => {
-  const okapiKy = useOkapiKy();
-  const [data, setData] = useState();
-  const [error, setError] = useState();
 
-  if (error) {
-    return <ErrorMessage message={error} />;
-  }
+const INITIAL_RESULT_COUNT = 10;
+const RESULT_COUNT_INCREMENT = 10;
 
-  if (!data) {
-    okapiKy('harvester-admin/harvestables')
-      .then(res => {
-        console.log('success:', res);
-        res.json().then(json => setData(json));
-      }).catch(err => {
-        console.log('failure:', err.toString());
-        err.response.text().then(text => {
-          setError(<><b>{err.toString()}</b>: {text}</>);
-        });
-      });
-
-    return <LoadingPane />;
-  }
-
-  return (
-    <>
-      <h1>HarvesterAdmin route</h1>
-      <pre>
-        {JSON.stringify(data, null, 2)}
-      </pre>
-    </>
-  );
+const sortMap = {
+  name: 'name',
+  id: 'id',
+  enabled: 'enabled',
+  jobClass: 'jobClass',
+  currentStatus: 'currentStatus',
 };
 
-export default HarvesterAdminRoute;
+const filterConfig = [];
+
+const searchableIndexes = [
+  'name',
+  'id',
+  'enabled',
+  'jobClass',
+  'currentStatus',
+];
+
+
+function HarvesterAdminRoute({ stripes, resources, mutator }) {
+  let [source, setSource] = useState(); // eslint-disable-line prefer-const
+  if (!source) {
+    source = new StripesConnectedSource({ resources, mutator }, stripes.logger, 'reportTitles');
+    setSource(source);
+  } else {
+    source.update({ resources, mutator }, 'reportTitles');
+  }
+
+  const handleNeedMoreData = () => source.fetchMore(RESULT_COUNT_INCREMENT);
+  const hasLoaded = resources.harvestables.hasLoaded; // XXX may need to inspect .url instead
+
+  return <HarvesterAdmin
+    data={{
+      harvestables: resources.harvestables.records,
+    }}
+    query={resources.query}
+    source={source}
+    mutator={mutator}
+    hasLoaded={hasLoaded}
+    onNeedMoreData={handleNeedMoreData}
+  />;
+}
+
+
+HarvesterAdminRoute.manifest = Object.freeze({
+  query: { initialValue: {} },
+  resultCount: { initialValue: INITIAL_RESULT_COUNT },
+  harvestables: {
+    type: 'okapi',
+    path: 'harvester-admin/harvestables',
+    records: 'harvestables',
+    recordsRequired: '%{resultCount}',
+    perRequest: RESULT_COUNT_INCREMENT,
+    params: {
+      query: makeQueryFunction(
+        'cql.allRecords=1',
+        searchableIndexes.map(index => `${index}="%{query.query}*"`).join(' or '),
+        sortMap,
+        filterConfig,
+      ),
+    },
+  },
+});
+
+
+HarvesterAdminRoute.propTypes = {
+  stripes: PropTypes.shape({
+    logger: PropTypes.object.isRequired,
+  }).isRequired,
+  resources: PropTypes.shape({
+    query: PropTypes.object.isRequired,
+    harvestables: PropTypes.shape({
+      hasLoaded: PropTypes.bool.isRequired,
+      records: PropTypes.arrayOf(
+        PropTypes.object.isRequired,
+      ),
+    }),
+  }).isRequired,
+  mutator: PropTypes.object,
+};
+
+
+export default stripesConnect(HarvesterAdminRoute);
---
+++ src/views/HarvesterAdmin.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { LoadingPane, MultiColumnList } from '@folio/stripes/components';
+import { SearchAndSortQuery } from '@folio/stripes/smart-components';
+
+
+function HarvesterAdmin({
+  data,
+  query, // eslint-disable-line no-unused-vars
+  source,
+  mutator, // eslint-disable-line no-unused-vars
+  hasLoaded,
+  onNeedMoreData
+}) {
+  const count = source ? source.totalCount() : 0;
+
+  if (!hasLoaded) return <LoadingPane />;
+
+  return (
+    <SearchAndSortQuery initialSearchState={{ query: '' }}>
+      {
+        (sasqParams) => {
+          return (
+            <MultiColumnList
+              id="list-harvestables"
+              virtualize
+              visibleColumns={['name', 'id', 'enabled', 'jobClass', 'currentStatus']}
+              columnMapping={{
+                name: <FormattedMessage id="ui-harvester-admin.harvestables.column.name" />,
+                id: <FormattedMessage id="ui-harvester-admin.harvestables.column.id" />,
+                enabled: <FormattedMessage id="ui-harvester-admin.harvestables.column.enabled" />,
+                jobClass: <FormattedMessage id="ui-harvester-admin.harvestables.column.jobClass" />,
+                currentStatus: <FormattedMessage id="ui-harvester-admin.harvestables.column.currentStatus" />,
+              }}
+              columnWidths={{
+                name: '400px',
+                id: '100px',
+                enabled: '100px',
+                jobClass: '150px',
+                currentStatus: '140px',
+              }}
+              formatter={{
+                jobClass: r => <FormattedMessage id={`ui-harvester-admin.harvestables.column.jobClass.${r.jobClass}`} />,
+              }}
+              contentData={data.harvestables}
+              totalCount={count}
+              onHeaderClick={sasqParams.onSort}
+              onNeedMoreData={onNeedMoreData}
+            />
+          );
+        }
+      }
+    </SearchAndSortQuery>
+  );
+}
+
+
+HarvesterAdmin.propTypes = {
+  data: PropTypes.shape({
+    harvestables: PropTypes.arrayOf(
+      PropTypes.shape({
+      }).isRequired,
+    ).isRequired,
+  }).isRequired,
+  query: PropTypes.object.isRequired,
+  source: PropTypes.shape({
+    totalCount: PropTypes.func.isRequired,
+  }),
+  mutator: PropTypes.shape({
+    query: PropTypes.shape({
+      update: PropTypes.func.isRequired,
+    }).isRequired,
+  }).isRequired,
+  hasLoaded: PropTypes.bool.isRequired,
+  onNeedMoreData: PropTypes.func.isRequired,
+};
+
+
+export default HarvesterAdmin;
