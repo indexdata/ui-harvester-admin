ead726366a8359a9176313a590e52196acccc97c
Mike Taylor <mike@miketaylor.org.uk>
2022-03-10 15:49:03 +0000

    Add optional `fieldName` prop to <CKV>

--- src/components/CKV.js
+++ src/components/CKV.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Row, Col, KeyValue } from '@folio/stripes/components';
 
-export const CKV = ({ rec, tag, xs }) => (
+export const CKV = ({ rec, tag, fieldName, xs }) => (
   <Col xs={xs}>
-    <KeyValue label={<FormattedMessage id={`ui-harvester-admin.harvestables.field.${tag}`} />} value={rec[tag]} />
+    <KeyValue label={<FormattedMessage id={`ui-harvester-admin.harvestables.field.${tag}`} />} value={rec[fieldName || tag]} />
   </Col>
 );
 
 CKV.propTypes = {
   rec: PropTypes.object.isRequired,
   tag: PropTypes.string.isRequired,
+  fieldName: PropTypes.string,
   xs: PropTypes.number.isRequired,
 };
 
