df825e4737f09621eb3ca6a13304f1a6c535dd78
Mike Taylor <mike@miketaylor.org.uk>
2022-03-10 13:24:38 +0000

    Move FullHarvestable.js down into its own directory

--- src/views/FullHarvestable/FullHarvestable.js
+++ src/views/FullHarvestable/FullHarvestable.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Loading, Accordion, Row, Col, KeyValue } from '@folio/stripes/components';
-import { CKV, RCKV } from '../components/CKV';
+import { CKV, RCKV } from '../../components/CKV';
 
 
 const FullHarvestable = ({ resource }) => {
