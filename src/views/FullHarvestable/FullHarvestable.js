2cb3e513dbbaede8d3cad84cabcd6e65019c60a7
Mike Taylor <mike@miketaylor.org.uk>
2022-03-11 16:53:12 +0000

    Omit general-info and status-info sections from Status harvestables

--- src/views/FullHarvestable/FullHarvestable.js
+++ src/views/FullHarvestable/FullHarvestable.js
@@ -27,66 +27,70 @@ const FullHarvestable = ({ resource }) => {
 
   return (
     <>
-      <Accordion
-        id="full-harvester-general"
-        label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
-      >
-        <RCKV rec={rec} tag="id" />
-        <RCKV rec={rec} tag="name" />
-        <RCKV rec={rec} tag="serviceProvider" />
-        <Row>
-          <CKV rec={rec} tag="usedBy" xs={6} />
-          <CKV rec={rec} tag="managedBy" xs={6} />
-        </Row>
-        <RCKV rec={rec} tag="openAccess" />
-        <RCKV rec={rec} tag="description" />
-        <RCKV rec={rec} tag="technicalNotes" />
-        <RCKV rec={rec} tag="contactNotes" />
-        <RCKV rec={rec} tag="enabled" />
-        <RCKV rec={rec} tag="scheduleString" />
-        <RCKV rec={rec} tag="transformationPipeline" />
-        <RCKV rec={rec} tag="laxParsing" />
-        <RCKV rec={rec} tag="encoding" />
-        <RCKV rec={rec} tag="storage.name" />
-        <RCKV rec={rec} tag="storageBatchLimit" />
-        <RCKV rec={rec} tag="cacheEnabled" />
-        <RCKV rec={rec} tag="storeOriginal" />
-        <RCKV rec={rec} tag="recordLimit" />
-        <RCKV rec={rec} tag="timeout" />
-        <RCKV rec={rec} tag="logLevel" />
-        <RCKV rec={rec} tag="failedRecordsLogging" />
-        <RCKV rec={rec} tag="maxSavedFailedRecordsPerRun" />
-        <RCKV rec={rec} tag="maxSavedFailedRecordsTotal" />
-        <RCKV rec={rec} tag="mailAddress" />
-        <RCKV rec={rec} tag="mailLevel" />
-        <RCKV rec={rec} tag="constantFields" />
-        <Row>
-          <Col xs={12}>
-            <KeyValue
-              label={<FormattedMessage id="ui-harvester-admin.harvestables.field.json" />}
-              value={<pre>{JSON.stringify(rec.json, null, 2)}</pre>}
-            />
-          </Col>
-        </Row>
-        <RCKV rec={rec} tag="id" />
-      </Accordion>
+      {type !== 'status' &&
+        <Accordion
+          id="full-harvester-general"
+          label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
+        >
+          <RCKV rec={rec} tag="id" />
+          <RCKV rec={rec} tag="name" />
+          <RCKV rec={rec} tag="serviceProvider" />
+          <Row>
+            <CKV rec={rec} tag="usedBy" xs={6} />
+            <CKV rec={rec} tag="managedBy" xs={6} />
+          </Row>
+          <RCKV rec={rec} tag="openAccess" />
+          <RCKV rec={rec} tag="description" />
+          <RCKV rec={rec} tag="technicalNotes" />
+          <RCKV rec={rec} tag="contactNotes" />
+          <RCKV rec={rec} tag="enabled" />
+          <RCKV rec={rec} tag="scheduleString" />
+          <RCKV rec={rec} tag="transformationPipeline" />
+          <RCKV rec={rec} tag="laxParsing" />
+          <RCKV rec={rec} tag="encoding" />
+          <RCKV rec={rec} tag="storage.name" />
+          <RCKV rec={rec} tag="storageBatchLimit" />
+          <RCKV rec={rec} tag="cacheEnabled" />
+          <RCKV rec={rec} tag="storeOriginal" />
+          <RCKV rec={rec} tag="recordLimit" />
+          <RCKV rec={rec} tag="timeout" />
+          <RCKV rec={rec} tag="logLevel" />
+          <RCKV rec={rec} tag="failedRecordsLogging" />
+          <RCKV rec={rec} tag="maxSavedFailedRecordsPerRun" />
+          <RCKV rec={rec} tag="maxSavedFailedRecordsTotal" />
+          <RCKV rec={rec} tag="mailAddress" />
+          <RCKV rec={rec} tag="mailLevel" />
+          <RCKV rec={rec} tag="constantFields" />
+          <Row>
+            <Col xs={12}>
+              <KeyValue
+                label={<FormattedMessage id="ui-harvester-admin.harvestables.field.json" />}
+                value={<pre>{JSON.stringify(rec.json, null, 2)}</pre>}
+              />
+            </Col>
+          </Row>
+          <RCKV rec={rec} tag="id" />
+        </Accordion>
+      }
 
       <SpecificSection rec={rec} />
 
-      <Accordion
-        id="full-harvester-general"
-        label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.status" />}
-      >
-        <Row>
-          <CKV rec={rec} tag="currentStatus" xs={4} />
-          <CKV rec={rec} tag="initiallyHarvested" xs={8} />
-        </Row>
-        <Row>
-          <CKV rec={rec} tag="lastHarvestStarted" xs={6} />
-          <CKV rec={rec} tag="lastHarvestFinished" xs={6} />
-        </Row>
-        <RCKV rec={rec} tag="lastUpdated" />
-      </Accordion>
+      {type !== 'status' &&
+        <Accordion
+          id="full-harvester-general"
+          label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.status" />}
+        >
+          <Row>
+            <CKV rec={rec} tag="currentStatus" xs={4} />
+            <CKV rec={rec} tag="initiallyHarvested" xs={8} />
+          </Row>
+          <Row>
+            <CKV rec={rec} tag="lastHarvestStarted" xs={6} />
+            <CKV rec={rec} tag="lastHarvestFinished" xs={6} />
+          </Row>
+          <RCKV rec={rec} tag="lastUpdated" />
+        </Accordion>
+      }
 
       <Accordion
         id="full-harvester-devinfo"
