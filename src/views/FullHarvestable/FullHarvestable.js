04d182f72db2697fb7af0a967eba36c8ff7c60df
Mike Taylor <mike@miketaylor.org.uk>
2022-03-21 15:41:56 +0000

    Move TrailerSection out into its own source file

--- src/views/FullHarvestable/FullHarvestable.js
+++ src/views/FullHarvestable/FullHarvestable.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Loading, Accordion, Row } from '@folio/stripes/components';
-import { CKV, RCKV } from '../../components/CKV';
+import { Loading, Accordion } from '@folio/stripes/components';
 import ErrorMessage from '../../components/ErrorMessage';
 import GeneralSection from './GeneralSection';
 import OaiPmhSection from './OaiPmhSection';
 import XmlBulkSection from './XmlBulkSection';
 import ConnectorSection from './ConnectorSection';
 import StatusSection from './StatusSection';
+import TrailerSection from './TrailerSection';
 
 
 const specificSections = {
@@ -29,25 +29,8 @@ const FullHarvestable = ({ resource }) => {
   return (
     <>
       {type !== 'status' && <GeneralSection rec={rec} />}
-
       <SpecificSection rec={rec} />
-
-      {type !== 'status' &&
-        <Accordion
-          id="full-harvester-general"
-          label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.status" />}
-        >
-          <Row>
-            <CKV rec={rec} tag="currentStatus" xs={6} />
-            <CKV rec={rec} tag="initiallyHarvested" xs={6} />
-          </Row>
-          <Row>
-            <CKV rec={rec} tag="lastHarvestStarted" xs={6} />
-            <CKV rec={rec} tag="lastHarvestFinished" xs={6} />
-          </Row>
-          <RCKV rec={rec} tag="lastUpdated" />
-        </Accordion>
-      }
+      {type !== 'status' && <TrailerSection rec={rec} />}
 
       <Accordion
         id="full-harvester-devinfo"
---
+++ src/views/FullHarvestable/TrailerSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Accordion, Row } from '@folio/stripes/components';
+import { RCKV, CKV } from '../../components/CKV';
+
+const TrailerSection = ({ rec }) => (
+  <Accordion
+    id="full-harvester-general"
+    label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.status" />}
+  >
+    <Row>
+      <CKV rec={rec} tag="currentStatus" xs={6} />
+      <CKV rec={rec} tag="initiallyHarvested" xs={6} />
+    </Row>
+    <Row>
+      <CKV rec={rec} tag="lastHarvestStarted" xs={6} />
+      <CKV rec={rec} tag="lastHarvestFinished" xs={6} />
+    </Row>
+    <RCKV rec={rec} tag="lastUpdated" />
+  </Accordion>
+);
+
+TrailerSection.propTypes = {
+  rec: PropTypes.object.isRequired,
+};
+
+export default TrailerSection;
