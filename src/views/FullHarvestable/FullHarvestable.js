307bd05251ab3a6a65b84316fe1c8da8f95370a0
Mike Taylor <mike@miketaylor.org.uk>
2022-03-21 15:34:00 +0000

    Move GeneralSection out into its own source file

--- src/views/FullHarvestable/FullHarvestable.js
+++ src/views/FullHarvestable/FullHarvestable.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Loading, Accordion, Row, Col, KeyValue } from '@folio/stripes/components';
+import { Loading, Accordion, Row } from '@folio/stripes/components';
 import { CKV, RCKV } from '../../components/CKV';
 import ErrorMessage from '../../components/ErrorMessage';
+import GeneralSection from './GeneralSection';
 import OaiPmhSection from './OaiPmhSection';
 import XmlBulkSection from './XmlBulkSection';
 import ConnectorSection from './ConnectorSection';
@@ -27,50 +28,7 @@ const FullHarvestable = ({ resource }) => {
 
   return (
     <>
-      {type !== 'status' &&
-        <Accordion
-          id="full-harvester-general"
-          label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
-        >
-          <RCKV rec={rec} tag="id" />
-          <RCKV rec={rec} tag="name" />
-          <RCKV rec={rec} tag="serviceProvider" />
-          <Row>
-            <CKV rec={rec} tag="usedBy" xs={6} />
-            <CKV rec={rec} tag="managedBy" xs={6} />
-          </Row>
-          <RCKV rec={rec} tag="openAccess" />
-          <RCKV rec={rec} tag="description" />
-          <RCKV rec={rec} tag="technicalNotes" />
-          <RCKV rec={rec} tag="contactNotes" />
-          <RCKV rec={rec} tag="enabled" />
-          <RCKV rec={rec} tag="scheduleString" />
-          <RCKV rec={rec} tag="transformationPipeline" />
-          <RCKV rec={rec} tag="laxParsing" />
-          <RCKV rec={rec} tag="encoding" />
-          <RCKV rec={rec} tag="storage.name" />
-          <RCKV rec={rec} tag="storageBatchLimit" />
-          <RCKV rec={rec} tag="cacheEnabled" />
-          <RCKV rec={rec} tag="storeOriginal" />
-          <RCKV rec={rec} tag="recordLimit" />
-          <RCKV rec={rec} tag="timeout" />
-          <RCKV rec={rec} tag="logLevel" />
-          <RCKV rec={rec} tag="failedRecordsLogging" />
-          <RCKV rec={rec} tag="maxSavedFailedRecordsPerRun" />
-          <RCKV rec={rec} tag="maxSavedFailedRecordsTotal" />
-          <RCKV rec={rec} tag="mailAddress" />
-          <RCKV rec={rec} tag="mailLevel" />
-          <RCKV rec={rec} tag="constantFields" />
-          <Row>
-            <Col xs={12}>
-              <KeyValue
-                label={<FormattedMessage id="ui-harvester-admin.harvestables.field.json" />}
-                value={<pre>{JSON.stringify(rec.json, null, 2)}</pre>}
-              />
-            </Col>
-          </Row>
-        </Accordion>
-      }
+      {type !== 'status' && <GeneralSection rec={rec} />}
 
       <SpecificSection rec={rec} />
 
---
+++ src/views/FullHarvestable/GeneralSection.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Accordion, Row, Col, KeyValue } from '@folio/stripes/components';
+import { RCKV, CKV } from '../../components/CKV';
+
+const GeneralSection = ({ rec }) => (
+  <Accordion
+    id="full-harvester-general"
+    label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
+  >
+    <RCKV rec={rec} tag="id" />
+    <RCKV rec={rec} tag="name" />
+    <RCKV rec={rec} tag="serviceProvider" />
+    <Row>
+      <CKV rec={rec} tag="usedBy" xs={6} />
+      <CKV rec={rec} tag="managedBy" xs={6} />
+    </Row>
+    <RCKV rec={rec} tag="openAccess" />
+    <RCKV rec={rec} tag="description" />
+    <RCKV rec={rec} tag="technicalNotes" />
+    <RCKV rec={rec} tag="contactNotes" />
+    <RCKV rec={rec} tag="enabled" />
+    <RCKV rec={rec} tag="scheduleString" />
+    <RCKV rec={rec} tag="transformationPipeline" />
+    <RCKV rec={rec} tag="laxParsing" />
+    <RCKV rec={rec} tag="encoding" />
+    <RCKV rec={rec} tag="storage.name" />
+    <RCKV rec={rec} tag="storageBatchLimit" />
+    <RCKV rec={rec} tag="cacheEnabled" />
+    <RCKV rec={rec} tag="storeOriginal" />
+    <RCKV rec={rec} tag="recordLimit" />
+    <RCKV rec={rec} tag="timeout" />
+    <RCKV rec={rec} tag="logLevel" />
+    <RCKV rec={rec} tag="failedRecordsLogging" />
+    <RCKV rec={rec} tag="maxSavedFailedRecordsPerRun" />
+    <RCKV rec={rec} tag="maxSavedFailedRecordsTotal" />
+    <RCKV rec={rec} tag="mailAddress" />
+    <RCKV rec={rec} tag="mailLevel" />
+    <RCKV rec={rec} tag="constantFields" />
+    <Row>
+      <Col xs={12}>
+        <KeyValue
+          label={<FormattedMessage id="ui-harvester-admin.harvestables.field.json" />}
+          value={<pre>{JSON.stringify(rec.json, null, 2)}</pre>}
+        />
+      </Col>
+    </Row>
+  </Accordion>
+);
+
+GeneralSection.propTypes = {
+  rec: PropTypes.object.isRequired,
+};
+
+export default GeneralSection;
