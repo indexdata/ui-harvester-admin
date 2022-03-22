542a329eee44c57c20090b458f6a1c4ccb852127
Mike Taylor <mike@miketaylor.org.uk>
2022-03-22 11:00:55 +0000

    Put general section of harvestable form into <Accordion>

--- src/forms/HarvestableForm/sections/HarvestableFormGeneral.js
+++ src/forms/HarvestableForm/sections/HarvestableFormGeneral.js
@@ -1,38 +1,45 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
-import { Col, Row, TextField } from '@folio/stripes/components';
+import { Accordion, Col, Row, TextField } from '@folio/stripes/components';
 
 const HarvestableFormGeneral = () => {
   return (
-    <div data-test-harvestable-info id="HarvestableFormGeneral">
-      <Row>
-        <Col xs={4}>
-          <FormattedMessage id="ui-harvester-admin.harvestables.field.id">
-            {placeholder => (
-              <Field
-                id="edit-harvestable-id"
-                name="id"
-                label={placeholder}
-                component={TextField}
-              />
-            )}
-          </FormattedMessage>
-        </Col>
-        <Col xs={4}>
-          <FormattedMessage id="ui-harvester-admin.harvestables.field.name">
-            {placeholder => (
-              <Field
-                id="edit-harvestable-name"
-                name="name"
-                label={placeholder}
-                component={TextField}
-              />
-            )}
-          </FormattedMessage>
-        </Col>
-      </Row>
-    </div>
+    <Accordion
+      id="full-harvester-form-general"
+      label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
+    >
+      <div data-test-harvestable-info id="HarvestableFormGeneral">
+        <Row>
+          <Col xs={4}>
+            <FormattedMessage id="ui-harvester-admin.harvestables.field.id">
+              {placeholder => (
+                <Field
+                  id="edit-harvestable-id"
+                  name="id"
+                  label={placeholder}
+                  component={TextField}
+                />
+              )}
+            </FormattedMessage>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4}>
+            <FormattedMessage id="ui-harvester-admin.harvestables.field.name">
+              {placeholder => (
+                <Field
+                  id="edit-harvestable-name"
+                  name="name"
+                  label={placeholder}
+                  component={TextField}
+                />
+              )}
+            </FormattedMessage>
+          </Col>
+        </Row>
+      </div>
+    </Accordion>
   );
 };
 
