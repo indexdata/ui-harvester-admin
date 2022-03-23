4fdb8055ce62100065274dd32497c7325aad0177
Mike Taylor <mike@miketaylor.org.uk>
2022-03-23 12:01:36 +0000

    Add OAI-PMH section boolean fields to list

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
-export const booleanFields = ['openAccess', 'enabled', 'laxParsing', 'cacheEnabled', 'storeOriginal'];
+export const booleanFields = [
+  // General section
+  'openAccess', 'enabled', 'laxParsing', 'cacheEnabled', 'storeOriginal',
+  // OAI-PMH section
+  'clearRtOnError', 'keepPartial',
+];
