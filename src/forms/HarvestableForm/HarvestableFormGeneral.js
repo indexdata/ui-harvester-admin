a9912b69b48d5c7b3aedf788bf9ebe0c3820462e
Mike Taylor <mike@miketaylor.org.uk>
2022-04-19 17:54:28 +0100

    Pure-functional component now uses a function

--- src/forms/HarvestableForm/HarvestableFormGeneral.js
+++ src/forms/HarvestableForm/HarvestableFormGeneral.js
@@ -12,42 +12,44 @@ const failedRecords = rawFailedRecords.map(x => ({
   label: x // <span>${x} - <FormattedMessage id={`harvestables.field.failedRecordsLogging.${x}`} /></span>
 }));
 
-const HarvestableFormGeneral = () => (
-  <Accordion
-    id="harvestable-form-general"
-    label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
-  >
-    <RCF tag="id" disabled />
-    <RCF tag="name" />
-    <RCF tag="serviceProvider" />
-    <Row>
-      <CF tag="usedBy" xs={6} />
-      <CF tag="managedBy" xs={6} />
-    </Row>
-    <RCF tag="openAccess" component={Checkbox} type="checkbox" />
-    <RCF tag="description" component={TextArea} rows="4" />
-    <RCF tag="technicalNotes" component={TextArea} rows="4" />
-    <RCF tag="contactNotes" component={TextArea} rows="4" />
-    <RCF tag="enabled" component={Checkbox} type="checkbox" />
-    <RCF tag="scheduleString" />
-    <RCF tag="transformationPipeline" />
-    <RCF tag="laxParsing" component={Checkbox} type="checkbox" />
-    <RCF tag="encoding" />
-    <RCF tag="storage.name" />
-    <RCF tag="storageBatchLimit" />
-    <RCF tag="cacheEnabled" component={Checkbox} type="checkbox" />
-    <RCF tag="storeOriginal" component={Checkbox} type="checkbox" />
-    <RCF tag="recordLimit" />
-    <RCF tag="timeout" />
-    <RCF tag="logLevel" component={Select} dataOptions={logLevels} />
-    <RCF tag="failedRecordsLogging" component={Select} dataOptions={failedRecords} />
-    <RCF tag="maxSavedFailedRecordsPerRun" />
-    <RCF tag="maxSavedFailedRecordsTotal" />
-    <RCF tag="mailAddress" />
-    <RCF tag="mailLevel" />
-    <RCF tag="constantFields" />
-    <RCF tag="json" component={TextArea} rows="6" />
-  </Accordion>
-);
+const HarvestableFormGeneral = () => {
+  return (
+    <Accordion
+      id="harvestable-form-general"
+      label={<FormattedMessage id="ui-harvester-admin.harvestables.heading.general" />}
+    >
+      <RCF tag="id" disabled />
+      <RCF tag="name" />
+      <RCF tag="serviceProvider" />
+      <Row>
+        <CF tag="usedBy" xs={6} />
+        <CF tag="managedBy" xs={6} />
+      </Row>
+      <RCF tag="openAccess" component={Checkbox} type="checkbox" />
+      <RCF tag="description" component={TextArea} rows="4" />
+      <RCF tag="technicalNotes" component={TextArea} rows="4" />
+      <RCF tag="contactNotes" component={TextArea} rows="4" />
+      <RCF tag="enabled" component={Checkbox} type="checkbox" />
+      <RCF tag="scheduleString" />
+      <RCF tag="transformationPipeline" />
+      <RCF tag="laxParsing" component={Checkbox} type="checkbox" />
+      <RCF tag="encoding" />
+      <RCF tag="storage.name" />
+      <RCF tag="storageBatchLimit" />
+      <RCF tag="cacheEnabled" component={Checkbox} type="checkbox" />
+      <RCF tag="storeOriginal" component={Checkbox} type="checkbox" />
+      <RCF tag="recordLimit" />
+      <RCF tag="timeout" />
+      <RCF tag="logLevel" component={Select} dataOptions={logLevels} />
+      <RCF tag="failedRecordsLogging" component={Select} dataOptions={failedRecords} />
+      <RCF tag="maxSavedFailedRecordsPerRun" />
+      <RCF tag="maxSavedFailedRecordsTotal" />
+      <RCF tag="mailAddress" />
+      <RCF tag="mailLevel" />
+      <RCF tag="constantFields" />
+      <RCF tag="json" component={TextArea} rows="6" />
+    </Accordion>
+  );
+};
 
 export default HarvestableFormGeneral;
