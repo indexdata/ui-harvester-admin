959f2dc7e366f1ac69d31ee204f55444cc8d25eb
Mike Taylor <mike@miketaylor.org.uk>
2022-04-25 17:25:14 +0100

    Incoming files for list editing

---
+++ src/forms/HarvestableForm/ViewCourseInstructors.js
@@ -0,0 +1,130 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import get from 'lodash/get';
+import { withStripes, CalloutContext } from '@folio/stripes/core';
+import { Button, Card, IconButton } from '@folio/stripes/components';
+import withOkapiKy from '../../../util/withOkapiKy';
+import css from './EditableList.css';
+
+
+const ViewCourseInstructors = (props) => {
+  const callout = useContext(CalloutContext);
+  function removeInstructor(instructorId) {
+    const oldCount = props.record.courseListingObject.instructorObjects.length;
+    const clid = props.record.courseListingId;
+    props.okapiKy(`coursereserves/courselistings/${clid}/instructors/${instructorId}`, {
+      method: 'DELETE',
+      headers: { Accept: 'text/plain' },
+    })
+      .text()
+      .then(() => { props.mutator.instructorCount.replace(oldCount - 1); })
+      .catch(exception => callout.sendCallout({
+        type: 'error',
+        message: <FormattedMessage id="ui-courses.removeInstructor.failure" values={{ message: exception }} />,
+      }));
+  }
+
+  const { record, stripes } = props;
+  const courseListingObject = record.courseListingObject || {};
+  const instructorObjects = (courseListingObject.instructorObjects || []).sort((a, b) => (
+    a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+  ));
+  const permissions = {
+    add: stripes.hasPerm('course-reserves-storage.courselistings.instructors.item.post'),
+    edit: stripes.hasPerm('course-reserves-storage.courselistings.instructors.item.put'),
+    delete: stripes.hasPerm('course-reserves-storage.courselistings.instructors.item.delete'),
+  };
+
+  const n = instructorObjects.length;
+
+  return (
+    <Card headerStart={<FormattedMessage id="ui-courses.instructor.pluralized" values={{ count: n }} />}>
+      {
+        n > 0 &&
+          <table className={css.instructors}>
+            <thead>
+              <tr>
+                {
+                  ['instructorName', 'barcode', 'patronGroup'].map(name => (
+                    <FormattedMessage key={name} id={`ui-courses.field.${name}`}>
+                      {label => <th align="left" aria-label={label}>{label}</th>}
+                    </FormattedMessage>
+                  ))
+                }
+              </tr>
+            </thead>
+            <tbody>
+              {
+                instructorObjects.map((instructor, index) => (
+                  <tr key={index}>
+                    <td>{instructor.name}</td>
+                    <td>{instructor.barcode}</td>
+                    <td>{get(instructor, 'patronGroupObject.group')}</td>
+                    <td>
+                      {
+                        permissions.edit && !instructor.patronGroupObject && (
+                          <FormattedMessage id="ui-courses.editInstructor">
+                            {ariaLabel => (
+                              <IconButton
+                                aria-label={ariaLabel}
+                                icon="edit"
+                                id={`clickable-edit-instructor-${index}`}
+                                to={`/cr/instructors/${courseListingObject.id}/${record.id}/${instructor.id}/edit`}
+                              />
+                            )}
+                          </FormattedMessage>
+                        )
+                      }
+                      {
+                        permissions.delete && (
+                          <FormattedMessage id="ui-courses.removeInstructor">
+                            {ariaLabel => (
+                              <IconButton
+                                aria-label={ariaLabel}
+                                icon="trash"
+                                id={`clickable-remove-instructor-${index}`}
+                                onClick={() => removeInstructor(instructor.id)}
+                              />
+                            )}
+                          </FormattedMessage>
+                        )
+                      }
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+      }
+      {permissions.add && (
+        <FormattedMessage id="ui-courses.addInstructor">
+          {ariaLabel => (
+            <Button
+              aria-label={ariaLabel}
+              id="clickable-add-instructor"
+              to={`/cr/instructors/${courseListingObject.id}/${record.id}/add`}
+            >
+              <FormattedMessage id="ui-courses.button.addInstructor" />
+            </Button>
+          )}
+        </FormattedMessage>
+      )}
+    </Card>
+  );
+};
+
+ViewCourseInstructors.propTypes = {
+  record: PropTypes.object.isRequired,
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+  }).isRequired,
+  mutator: PropTypes.shape({
+    instructorCount: PropTypes.shape({
+      replace: PropTypes.func.isRequired,
+    }).isRequired,
+  }).isRequired,
+  okapiKy: PropTypes.func.isRequired,
+};
+
+export default withOkapiKy(withStripes(ViewCourseInstructors));
