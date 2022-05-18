74d19394491a5be13bd15c2badecf6c9d75b7231
Mike Taylor <mike@miketaylor.org.uk>
2022-05-18 21:24:00 +0100

    Get OAI-PMH sets from nominated service

--- src/forms/HarvestableForm/HarvestableFormOaiPmh.js
+++ src/forms/HarvestableForm/HarvestableFormOaiPmh.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Accordion, Row, Checkbox, Datepicker } from '@folio/stripes/components';
+import OaiPmh from '@indexdata/oai-pmh';
+import { Accordion, Row, Checkbox, Datepicker, Select } from '@folio/stripes/components';
 import { RCF, CF } from '../../components/CF';
 
-const HarvestableFormOaiPmh = () => {
+const HarvestableFormOaiPmh = ({ values }) => {
+  const [sets, setSets] = useState();
+  useEffect(() => {
+    async function fetchOaiPmhData() {
+      const oaiPhm = new OaiPmh(values.url);
+      let setsResponse;
+      try {
+        setsResponse = await oaiPhm.listSets();
+      } catch (err) {
+        // It would be nice to do something cleverer here
+        // eslint-disable-next-line no-console
+        console.error('OAI-PMH oops!', err);
+        return;
+      }
+      setSets(setsResponse.ListSets[0].set.map(x => ({
+        value: x.setSpec[0],
+        label: `${x.setName} (${x.setSpec[0]})`,
+      })));
+    }
+    fetchOaiPmhData();
+  }, [values.url]);
+
   return (
     <Accordion
       id="harvestable-form-oai"
       label={<FormattedMessage id="ui-harvester-admin.harvestables.field.type.oaiPmh" />}
     >
       <RCF tag="url" />
-      <RCF tag="oaiSetName" />
+      <RCF tag="oaiSetName" component={Select} dataOptions={sets} />
       <RCF tag="metadataPrefix" />
       <RCF tag="dateFormat" i18nTag="useLongDateFormat" component={Checkbox} type="checkbox" />
       <Row>
@@ -26,6 +49,12 @@ const HarvestableFormOaiPmh = () => {
       </Row>
     </Accordion>
   );
+};
+
+HarvestableFormOaiPmh.propTypes = {
+  values: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default HarvestableFormOaiPmh;
