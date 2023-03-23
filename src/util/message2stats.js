e0c7ece6dff743008d314c851cfca31ed8c2894b
Mike Taylor <mike@indexdata.com>
2023-03-23 22:45:39 +0000

    Initial version of logs header

---
+++ src/util/message2stats.js
@@ -0,0 +1,25 @@
+function message2stats(message) {
+  const stats = {};
+  message.split(' ').forEach(row => {
+    const matchData = row.match(/(.*?)_.*:_+([^_]*)_+([^_]*)_+([^_]*)_+([^_]*)/);
+    if (matchData) {
+      // eslint-disable-next-line no-unused-vars
+      const [_matched, name, processed, loaded, deleted, failed] = matchData;
+      stats[name.toLowerCase()] = { processed, loaded, deleted, failed };
+    } else {
+      // eslint-disable-next-line no-console
+      console.warn('message2stats: no match for row:', row);
+    }
+  });
+
+  return stats;
+}
+
+/*
+const message = 'Instances_processed/loaded/deletions(signals)/failed:__0___0___0(0)___0_ Holdings_records_processed/loaded/deleted/failed:__107___0___0___107_ Items_processed/loaded/deleted/failed:__107___0___0___0_ Source_records_processed/loaded/deleted/failed:__0___0___0___0_';
+
+const stats = message2stats(message);
+console.log(stats);
+*/
+
+export default message2stats;
--- src/views/HarvestableLogs/HarvestableLogsHeader.js
+++ src/views/HarvestableLogs/HarvestableLogsHeader.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { FormattedMessage } from 'react-intl';
-import {} from '@folio/stripes/components';
+import { FormattedMessage } from 'react-intl';
+import { MultiColumnList, Accordion } from '@folio/stripes/components';
+import message2stats from '../../util/message2stats';
 
 
 const HarvestableLogsHeader = ({ harvestable }) => {
+  const stats = message2stats(harvestable.message);
+
+  const visibleColumns = ['summary', 'source', 'instances', 'holdings', 'items'];
+
+  const columnMapping = {
+    summary: <FormattedMessage id="ui-harvester-admin.summary-table.summary" />,
+    source: <FormattedMessage id="ui-harvester-admin.summary-table.source" />,
+    instances: <FormattedMessage id="ui-harvester-admin.summary-table.instances" />,
+    holdings: <FormattedMessage id="ui-harvester-admin.summary-table.holdings" />,
+    items: <FormattedMessage id="ui-harvester-admin.summary-table.items" />,
+  };
+
+  const contentData = ['processed', 'loaded', 'deleted', 'failed'].map(caption => {
+    const values = {
+      summary: <FormattedMessage id={`ui-harvester-admin.summary-label.${caption}`} />,
+    };
+    visibleColumns.slice(1).forEach(col => {
+      values[col] = stats[col]?.[caption];
+    });
+    return values;
+  });
+
+  const resultsFormatter = {};
+
   return (
-    <pre>
-      {harvestable.message.replace(/ /g, '\n')}
-    </pre>
+    <>
+      <MultiColumnList
+        id="harvest-summary-table"
+        columnIdPrefix="harvest-summary-table"
+        visibleColumns={visibleColumns}
+        columnMapping={columnMapping}
+        columnWidths={{}}
+        contentData={contentData}
+        formatter={resultsFormatter}
+      />
+      <Accordion
+        id="harvest-summary-devinfo"
+        label={<FormattedMessage id="ui-harvester-admin.accordion.devinfo" />}
+        closedByDefault
+      >
+        <pre>
+          {harvestable.message.replace(/ /g, '\n')}
+          <br />
+          <br />
+          {JSON.stringify(stats, null, 2)}
+        </pre>
+      </Accordion>
+    </>
   );
 };
 
