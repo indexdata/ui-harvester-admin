a1311843285eaed18608e9358d56ab6ca3414d92
Mike Taylor <mike@indexdata.com>
2023-03-23 14:34:50 +0000

    Very basic header

--- src/views/HarvestableLogs/HarvestableLogsHeader.js
+++ src/views/HarvestableLogs/HarvestableLogsHeader.js
@@ -1,1 +1,23 @@
-export default () => 'header';
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { FormattedMessage } from 'react-intl';
+import {} from '@folio/stripes/components';
+
+
+const HarvestableLogsHeader = ({ harvestable }) => {
+  return (
+    <pre>
+      {harvestable.message.replace(/ /g, '\n')}
+    </pre>
+  );
+};
+
+
+HarvestableLogsHeader.propTypes = {
+  harvestable: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+
+export default HarvestableLogsHeader;
