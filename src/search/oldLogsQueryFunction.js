8d2acec301740a6b88f6f973278be7a883ed9721
Mike Taylor <mike@indexdata.com>
2023-04-03 18:27:27 +0100

    Fold oldLogsFilterConfig.js and oldLogsSortMap.js into oldLogsQueryFunction.js
    It turns out this is the only place they're used.

--- src/search/oldLogsQueryFunction.js
+++ src/search/oldLogsQueryFunction.js
@@ -1,7 +1,25 @@
 import { makeQueryFunction } from '@folio/stripes/smart-components';
 import indexNames from './oldLogsIndexNames';
-import sortMap from './oldLogsSortMap';
-import filterConfig from './oldLogsFilterConfig';
+
+const sortMap = {
+  // XXX I _think_ all the headings are the names of sortable fields
+  // Verify this when server-side sorting starts to work
+};
+
+const filterConfig = [{
+  name: 'status',
+  cql: 'status',
+  values: [],
+}, {
+  name: 'type',
+  cql: 'type',
+  values: [],
+}, {
+  // Value is injected from path-component by HarvestableOldLogsRoute's query function
+  name: 'harvestableId',
+  cql: 'harvestableId',
+  values: [],
+}];
 
 const oldLogsqueryFunction = makeQueryFunction(
   'cql.allRecords=1',
--- src/search/oldLogsSortMap.js
+++
@@ -1,6 +0,0 @@
-const oldLogsSortMap = {
-  // XXX I _think_ all the headings are the names of sortable fields
-  // Verify this when server-side sorting starts to work
-};
-
-export default oldLogsSortMap;
