d9be63690f692f6a4ac21123813092ef91ee4145
Mike Taylor <mike@indexdata.com>
2023-04-25 11:43:52 +0100

    Simplify dumb version of Records.js down to its basics

--- src/views/Records.js
+++ src/views/Records.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { useIntl, FormattedMessage } from 'react-intl';
-// import { AppIcon } from '@folio/stripes/core';
 import { LoadingPane } from '@folio/stripes/components';
-// import { LoadingPane, Paneset, Pane, MultiColumnList, ErrorModal } from '@folio/stripes/components';
-// import { ColumnManager, SearchAndSortQuery } from '@folio/stripes/smart-components';
-// import parseSort from '../util/parseSort';
-// import formatDateTime from '../util/formatDateTime';
-// import { message2stats, summarizeStats } from '../util/message2stats';
-// import RecordsSearchPane from '../search/JobsSearchPane';
 import ErrorMessage from '../components/ErrorMessage';
 import HarvestableLogFailedRecords from './HarvestableLog/HarvestableLogFailedRecords';
-// import packageInfo from '../../package';
 
 
 function Records({
   data,
-  // query,
-  // resultCount,
-  // updateQuery,
   error,
   hasLoaded,
-  // onNeedMoreData,
-  // children,
 }) {
-  // const intl = useIntl();
   if (error) return <ErrorMessage message={error} />;
   if (!hasLoaded) return <LoadingPane />;
 
@@ -43,21 +28,11 @@ function Records({
 Records.propTypes = {
   data: PropTypes.shape({
     records: PropTypes.arrayOf(
-      PropTypes.shape({
-        // XXX fill in
-      }).isRequired,
+      PropTypes.shape({}).isRequired,
     ).isRequired,
   }).isRequired,
-  // query: PropTypes.object.isRequired,
-  // resultCount: PropTypes.number,
-  // updateQuery:PropTypes.func.isRequired,
   error: PropTypes.string,
   hasLoaded: PropTypes.bool.isRequired,
-  // onNeedMoreData: PropTypes.func.isRequired,
-  // children: PropTypes.oneOfType([
-  //   PropTypes.object.isRequired,
-  //   PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
-  // ]),
 };
 
 
