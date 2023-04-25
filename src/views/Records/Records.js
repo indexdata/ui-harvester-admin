3966b8637330be48d336e91a278a5c0f0d678b77
Mike Taylor <mike@indexdata.com>
2023-04-25 11:47:13 +0100

    Move Records,js down into own directory

--- src/views/Records/Records.js
+++ src/views/Records/Records.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { LoadingPane } from '@folio/stripes/components';
-import ErrorMessage from '../components/ErrorMessage';
-import HarvestableLogFailedRecords from './HarvestableLog/HarvestableLogFailedRecords';
+import ErrorMessage from '../../components/ErrorMessage';
+import HarvestableLogFailedRecords from '../HarvestableLog/HarvestableLogFailedRecords';
 
 
 function Records({
