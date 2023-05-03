c9fe52eafd777407f7deb0a460c2408fa3dc418b
Mike Taylor <mike@indexdata.com>
2023-05-03 17:10:48 +0100

    Failed-records page: default to today's reports
    Fixes UIHAADM-57.

--- src/routes/SwitchRoute.js
+++ src/routes/SwitchRoute.js
@@ -3,22 +3,33 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Button, ButtonGroup } from '@folio/stripes/components';
 
-const segments = ['harvestables', 'jobs', 'records'];
-
 function SwitchRoute({ location, children }) {
+  const today = new Date();
+  const yesterday = new Date(today - 24*60*60*1000);
+  const isoString = yesterday.toISOString().substring(0, 10);
+
+  const segments = [{
+    name: 'harvestables',
+  }, {
+    name: 'jobs',
+  }, {
+    name: 'records',
+    params: `filters=timeStamp_from.${isoString}`,
+  }];
+
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '0.5em' }}>
         <ButtonGroup data-test-navigation>
           {
-            segments.map(name => {
+            segments.map(({ name, params }) => {
               let effectiveTab = location.pathname.replace(/^\/ha\//, '').replace(/\/.*/, '');
               if (location.pathname.endsWith('/jobs')) effectiveTab = 'jobs';
               const selected = (effectiveTab === name);
               return (
                 <Button
                   key={`${name}`}
-                  to={`/ha/${name}`}
+                  to={`/ha/${name}${params ? `?${params}` : ''}`}
                   buttonStyle={selected ? 'primary' : 'default'}
                   aria-selected={selected}
                 >
