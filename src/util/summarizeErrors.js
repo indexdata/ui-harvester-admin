992b84eb9985c1a1f5522544630348717536b09c
Mike Taylor <mike@indexdata.com>
2023-05-26 10:37:29 +0100

    Error-summarising function handles more structures
    Fixes UIHAADM-72.

--- src/util/summarizeErrors.js
+++ src/util/summarizeErrors.js
@@ -5,13 +5,19 @@ function summarizeErrors(errors) {
   return (
     <ul className={css.noDot}>
       {
-        errors.map(error => (
-          error.error?.message?.errors.map(x => (
-            <li>
-              {x.message}
-            </li>
-          ))
-        ))
+        errors.map(error => {
+          const message = error.error?.message;
+          const errorList = message?.errors;
+          const mm = message.message;
+
+          if (errorList) {
+            return errorList.map(x => <li>{x.message}</li>);
+          } else if (mm) {
+            return mm;
+          } else {
+            return error.error?.label;
+          }
+        })
       }
     </ul>
   );
