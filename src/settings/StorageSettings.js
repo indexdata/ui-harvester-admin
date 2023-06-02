4da7efb55294ad3867d1209621fe6ca7c3cb48a6
Mike Taylor <mike@indexdata.com>
2023-06-02 21:21:54 +0100

    component -> functional

--- src/settings/StorageSettings.js
+++ src/settings/StorageSettings.js
@@ -8,60 +8,52 @@ import { stripesConnect } from '@folio/stripes/core';
 import StorageDetail from './StorageDetail';
 import StorageForm from './StorageForm';
 
-class StorageSettings extends React.Component {
-  static propTypes = {
-    resources: PropTypes.shape({
-      entries: PropTypes.shape({
-        records: PropTypes.arrayOf(PropTypes.object),
-      }),
-    }).isRequired,
-    mutator: PropTypes.shape({
-      entries: PropTypes.shape({
-        POST: PropTypes.func,
-        PUT: PropTypes.func,
-        DELETE: PropTypes.func,
-      }),
-    }).isRequired,
-    intl: PropTypes.object.isRequired,
-  };
+const StorageSettings = (props) => {
+  const { mutator, resources, intl } = props;
 
-  static manifest = Object.freeze({
-    entries: {
-      type: 'okapi',
-      records: 'storages',
-      path: 'harvester-admin/storages',
-      throwErrors: false,
-    },
-  });
+  return (
+    <EntryManager
+      {...props}
+      parentMutator={mutator}
+      entryList={sortBy((resources.entries || {}).records || [], ['name'])}
+      detailComponent={StorageDetail}
+      paneTitle={intl.formatMessage({ id: 'ui-harvester-admin.settings.storage' })}
+      entryLabel={intl.formatMessage({ id: 'ui-harvester-admin.settings.storage.heading' })}
+      entryFormComponent={StorageForm}
+      nameKey="name"
+      permissions={{
+        // XXX Change these when we resolve the permissions plan for Harvester Admin
+        put: 'inventory-storage.instances.collection.get',
+        post: 'inventory-storage.instances.collection.get',
+        delete: 'inventory-storage.instances.collection.get',
+      }}
+    />
+  );
+};
 
-  render() {
-    const {
-      mutator,
-      resources,
-      intl: {
-        formatMessage,
-      },
-    } = this.props;
+StorageSettings.propTypes = {
+  resources: PropTypes.shape({
+    entries: PropTypes.shape({
+      records: PropTypes.arrayOf(PropTypes.object),
+    }),
+  }).isRequired,
+  mutator: PropTypes.shape({
+    entries: PropTypes.shape({
+      POST: PropTypes.func,
+      PUT: PropTypes.func,
+      DELETE: PropTypes.func,
+    }),
+  }).isRequired,
+  intl: PropTypes.object.isRequired,
+};
 
-    return (
-      <EntryManager
-        {...this.props}
-        parentMutator={mutator}
-        entryList={sortBy((resources.entries || {}).records || [], ['name'])}
-        detailComponent={StorageDetail}
-        paneTitle={formatMessage({ id: 'ui-harvester-admin.settings.storage' })}
-        entryLabel={formatMessage({ id: 'ui-harvester-admin.settings.storage.heading' })}
-        entryFormComponent={StorageForm}
-        nameKey="name"
-        permissions={{
-          // XXX Change these when we resolve the permissions plan for Harvester Admin
-          put: 'inventory-storage.instances.collection.get',
-          post: 'inventory-storage.instances.collection.get',
-          delete: 'inventory-storage.instances.collection.get',
-        }}
-      />
-    );
-  }
-}
+StorageSettings.manifest = Object.freeze({
+  entries: {
+    type: 'okapi',
+    records: 'storages',
+    path: 'harvester-admin/storages',
+    throwErrors: false,
+  },
+});
 
 export default stripesConnect(injectIntl(StorageSettings));
