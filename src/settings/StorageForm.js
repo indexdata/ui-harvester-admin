541089269e2cd912c46fd969050cebee62302a4e
Mike Taylor <mike@indexdata.com>
2023-06-05 19:47:09 +0100

    Save type

--- src/settings/StorageForm.js
+++ src/settings/StorageForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { useIntl, FormattedMessage } from 'react-intl';
 import arrayMutators from 'final-form-arrays';
-import { Button, Pane, PaneFooter, Row, Checkbox, TextArea } from '@folio/stripes/components';
+import { Button, Pane, PaneFooter, Row, Select, Checkbox, TextArea } from '@folio/stripes/components';
 import { TitleManager } from '@folio/stripes/core';
 import stripesFinalForm from '@folio/stripes/final-form';
 import { isEqual } from 'lodash';
@@ -13,9 +13,13 @@ import { RCF, CF } from '../components/CF';
 function validate(values) {
   const errors = {};
   const requiredTextMessage = <FormattedMessage id="ui-harvester-admin.fillIn" />;
+  const requiredSelectMessage = <FormattedMessage id="ui-harvester-admin.selectToContinue" />;
 
   if (!values.name) {
     errors.name = requiredTextMessage;
+  }
+  if (!values.type) {
+    errors.type = requiredSelectMessage;
   }
 
   return errors;
@@ -24,6 +28,13 @@ function validate(values) {
 
 const StorageForm = (props) => {
   const { handleSubmit, onCancel, pristine, submitting } = props;
+  const intl = useIntl();
+
+  const noValue = {
+    value: '',
+    label: intl.formatMessage({ id: 'ui-harvester-admin.selectValue' }),
+  };
+  const types = ['inventoryStorage', 'solrStorage'].map(x => ({ value: x, label: x }));
 
   function renderPaneFooter() {
     return (
@@ -67,11 +78,12 @@ const StorageForm = (props) => {
       <TitleManager record={title}>
         <form id="form-course">
           <Row>
-            <CF tag="id" xs={3} disabled />
-            <CF tag="name" xs={9} required />
+            <CF tag="id" xs={2} disabled />
+            <CF tag="type" domain="storage" xs={2} component={Select} dataOptions={[noValue].concat(types)} required />
+            <CF tag="name" xs={8} required />
           </Row>
-          <RCF tag="description" component={TextArea} rows="4" />
-          <RCF tag="enabled" component={Checkbox} type="checkbox" />
+          <RCF tag="description" domain="storage" component={TextArea} rows="4" />
+          <RCF tag="enabled" domain="storage" component={Checkbox} type="checkbox" />
           <RCF tag="url" />
           <RCF tag="json" component={TextArea} rows="4" />
         </form>
