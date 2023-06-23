19f3247aa0a29c40d66fad2cb5e01ac7cae44d1a
Mike Taylor <mike@indexdata.com>
2023-06-23 17:16:59 +0100

    Do not display tabs that user lacks permissions for

--- src/routes/SwitchRoute.js
+++ src/routes/SwitchRoute.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { useStripes } from '@folio/stripes/core';
 import { Button, ButtonGroup } from '@folio/stripes/components';
 
 function SwitchRoute({ location, children }) {
+  const stripes = useStripes();
   const today = new Date();
   const yesterday = new Date(today - 24 * 60 * 60 * 1000);
   const isoString = yesterday.toISOString().substring(0, 10);
 
   const segments = [{
     name: 'harvestables',
+    perm: 'ui-harvester-admin.harvestables.view',
   }, {
     name: 'jobs',
+    perm: 'ui-harvester-admin.jobs-and-failed-records.view',
   }, {
     name: 'records',
+    perm: 'ui-harvester-admin.jobs-and-failed-records.view',
     params: `filters=timeStamp_from.${isoString}`,
   }];
 
@@ -22,7 +27,7 @@ function SwitchRoute({ location, children }) {
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '0.5em' }}>
         <ButtonGroup data-test-navigation>
           {
-            segments.map(({ name, params }) => {
+            segments.filter(({ perm }) => stripes.hasPerm(perm)).map(({ name, params }) => {
               let effectiveTab = location.pathname.replace(/^\/ha\//, '').replace(/\/.*/, '');
               if (location.pathname.endsWith('/jobs')) effectiveTab = 'jobs';
               const selected = (effectiveTab === name);
