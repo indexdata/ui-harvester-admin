9ad63b016afa07118884e6e4084aea69ab822729
Mike Taylor <mike@indexdata.com>
2023-07-21 10:13:15 +0100

    <ConnectedWrapper> honours clonedRecordId

--- src/smart-components/lib/EntryManager/ConnectedWrapper.js
+++ src/smart-components/lib/EntryManager/ConnectedWrapper.js
@@ -3,23 +3,28 @@ import PropTypes from 'prop-types';
 import { useOkapiKy } from '@folio/stripes/core';
 import { Loading } from '@folio/stripes/components';
 
-function ConnectedWrapper({ resourcePath, initialValues, underlyingComponent, ...rest }) {
+function ConnectedWrapper({ resourcePath, initialValues, underlyingComponent, clonedRecordId, ...rest }) {
   const okapiKy = useOkapiKy();
   const [record, setRecord] = useState();
   const Component = underlyingComponent;
+  const idToLoad = clonedRecordId || initialValues?.id;
 
+  console.log(`in ConnectedWrapper: clonedRecordId=${clonedRecordId}, idToLoad=${idToLoad}`);
   useEffect(() => {
-    if (initialValues?.id) {
-      okapiKy(`${resourcePath}/${initialValues.id}`)
+    console.log(' in ConnectedWrapper: useEffect');
+    if (idToLoad) {
+      console.log('  in ConnectedWrapper: fetching');
+      okapiKy(`${resourcePath}/${idToLoad}`)
         .then(res => res.json().then(rec => {
+          console.log('   in ConnectedWrapper: setting record', rec);
           setRecord(rec);
         }));
     }
     // If `okpaiKy` is included in the dependency list, the effect fires on every render.
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initialValues?.id, resourcePath]);
+  }, [idToLoad, resourcePath]);
 
-  if (!initialValues?.id) {
+  if (!idToLoad) {
     // Creating a new record: pass through
     return <Component initialValues={initialValues} {...rest} />;
   }
@@ -37,6 +42,7 @@ ConnectedWrapper.propTypes = {
     id: PropTypes.string,
   }), // This is .isRequired really, but React sometimes complains that it's undefined when this component isn't even rendered
   underlyingComponent: PropTypes.func, // React component
+  clonedRecordId: PropTypes.string,
 };
 
 export default ConnectedWrapper;
