049140463a231e196d632cbe51b338c06eb7d171
Mike Taylor <mike@indexdata.com>
2024-02-01 11:40:04 +0000

    Separate XSLT compilation and rendering status

--- src/settings/ScriptOK.js
+++ src/settings/ScriptOK.js
@@ -1,38 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { compileXSLT, NO_TEXT, BAD_XML, BAD_XSLT } from './compileXSLT';
 import css from './ScriptOK.css';
 
 const ScriptOK = ({ xslText }) => {
-  if (xslText === '') {
+  const [status, value] = compileXSLT(xslText);
+
+  if (status === NO_TEXT) {
     return null;
-  }
-
-  const parser = new DOMParser();
-  const xsltProcessor = new XSLTProcessor();
-
-  const xslStylesheet = parser.parseFromString(xslText, 'application/xml');
-  const errorNode = xslStylesheet.querySelector('parsererror');
-  if (errorNode) {
-    let errorMessage = errorNode.textContent.trim();
-    // This is often of the form:
-    // XML Parsing Error: syntax error Location: http://localhost:3000/settings/ha/step/10010?layer=edit Line Number 1, Column 1
-    errorMessage = errorMessage.replace(/Location: https?:[^ ]+/, '');
-    return (
-      <div className={css.badXML}>
-        <FormattedMessage id="ui-harvester-admin.invalidXML" values={{ error: errorMessage }} />
-      </div>
-    );
-  }
-
-  try {
-    xsltProcessor.importStylesheet(xslStylesheet);
-  } catch (e) {
-    // No value to the exception: it's effectively a boolean (thrown or not)
-    // The XML was not valid XSLT
+  } else if (status === BAD_XML) {
+    return <div className={css.badXML}><FormattedMessage id="ui-harvester-admin.invalidXML" values={{ error: value }} /></div>;
+  } else if (status === BAD_XSLT) {
     return <div className={css.badXSLT}><FormattedMessage id="ui-harvester-admin.invalidXSLT" /></div>;
   }
 
+  // It's good XSLT
   return <div className={css.good}><FormattedMessage id="ui-harvester-admin.validXSLT" /></div>;
 };
 
---
+++ src/settings/compileXSLT.js
@@ -0,0 +1,43 @@
+const NO_TEXT = 1;
+const BAD_XML = 2;
+const BAD_XSLT = 3;
+const GOOD_XSLT = 4;
+
+// Tries to compile `xslText` into an XSLT processor configured with an executable stylesheet.
+// Returns a tuple of [status, value].
+// Status can be:
+//      NO_TEXT -- text is empty, so there is no stylesheet to compile
+//      BAD_XML -- text is provided but cannot be parsed as XML: value is error message
+//      BAD_XSLT -- text is valid XML, but is not an XSLT stylesheet
+//      GOOD_XSLT -- text is a valid XSLT stylesheet: value is the XSLT processor
+// There is no value for the NO_TEXT and BAD_XSLT statuses.
+//
+function compileXSLT(xslText) {
+  if (xslText === '') {
+    return [NO_TEXT];
+  }
+
+  const parser = new DOMParser();
+  const xsltProcessor = new XSLTProcessor();
+
+  const xslStylesheet = parser.parseFromString(xslText, 'application/xml');
+  const errorNode = xslStylesheet.querySelector('parsererror');
+  if (errorNode) {
+    let errorMessage = errorNode.textContent.trim();
+    // This is often of the form:
+    // XML Parsing Error: syntax error Location: http://localhost:3000/settings/ha/step/10010?layer=edit Line Number 1, Column 1
+    errorMessage = errorMessage.replace(/Location: https?:[^ ]+/, '');
+    return [BAD_XML, errorMessage];
+  }
+
+  try {
+    xsltProcessor.importStylesheet(xslStylesheet);
+  } catch (e) {
+    // No value to the exception: it's effectively a boolean (thrown or not)
+    return [BAD_XSLT];
+  }
+
+  return [GOOD_XSLT, xsltProcessor];
+}
+
+export { compileXSLT, NO_TEXT, BAD_XML, BAD_XSLT, GOOD_XSLT };
