f287d28a2112c5bc0930cab6850d78750c7a8ad8
Mike Taylor <mike@indexdata.com>
2024-02-01 12:03:48 +0000

    Remove ScriptOK.js, rename CSS file

--- src/settings/StepForm.js
+++ src/settings/StepForm.js
@@ -10,7 +10,7 @@ import setFieldData from 'final-form-set-field-data'; // XXX do we need this?
 import { RCF, CF } from '../components/CF';
 import renderPaneFooter from './renderPaneFooter';
 import { compileXSLT, BAD_XML, BAD_XSLT, GOOD_XSLT } from './compileXSLT';
-import css from './ScriptOK.css';
+import css from './StepForm.css';
 
 
 function validate(values) {
