f67ff61f9d9a888f622f54e1f4c5726d7e58b6b6
Mike Taylor <mike@indexdata.com>
2024-02-01 00:42:07 +0000

    Internationalize XSLT validation messages

--- src/settings/ScriptOK.js
+++ src/settings/ScriptOK.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import css from './ScriptOK.css';
 
 const ScriptOK = ({ xslText }) => {
@@ -17,7 +18,11 @@ const ScriptOK = ({ xslText }) => {
     // This is often of the form:
     // XML Parsing Error: syntax error Location: http://localhost:3000/settings/ha/step/10010?layer=edit Line Number 1, Column 1
     errorMessage = errorMessage.replace(/Location: https?:[^ ]+/, '');
-    return <div className={css.badXML}>Bad XML: {errorMessage}</div>;
+    return (
+      <div className={css.badXML}>
+        <FormattedMessage id="ui-harvester-admin.invalidXML" values={{ error: errorMessage }} />
+      </div>
+    );
   }
 
   try {
@@ -25,10 +30,10 @@ const ScriptOK = ({ xslText }) => {
   } catch (e) {
     // No value to the exception: it's effectively a boolean (thrown or not)
     // The XML was not valid XSLT
-    return <div className={css.badXSLT}>Valid XML, but not an XSLT stylesheet</div>;
+    return <div className={css.badXSLT}><FormattedMessage id="ui-harvester-admin.invalidXSLT" /></div>;
   }
 
-  return <div className={css.good}>Valid XSLT stylesheet</div>;
+  return <div className={css.good}><FormattedMessage id="ui-harvester-admin.validXSLT" /></div>;
 };
 
 ScriptOK.propTypes = {
