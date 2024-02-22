e924256333a2cfd3ad25b0085f6df3a99c99f131
Mike Taylor <mike@miketaylor.org.uk>
2024-02-22 12:44:16 +0000

    Improve the situation with keeping all tabs on the page (#67)
    We are now down to two scrollbars instead of three(!), and the Harvestables/Jobs/Failed Records tabs are now always on screen. But they can still be scrolled upwards and the FOLIO top-bar scrolled off the screen. This is not as good as the situation in the Agreements application, where things work as they ought.
    
    I will return to this later and try to complete the work by eliminating the second scrollbar.
    
    The present commit addresses but does not fully fix UIHAADM-99.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Switch, Redirect } from 'react-router-dom';
 import { Route as NestedRoute } from '@folio/stripes/core';
 import Settings from './settings';
-import SwitchRoute from './routes/SwitchRoute';
 import HarvestablesRoute from './routes/HarvestablesRoute';
 import CreateHarvestableRoute from './routes/CreateHarvestableRoute';
 import FullHarvestableRoute from './routes/FullHarvestableRoute';
@@ -13,6 +12,10 @@ import HarvestableJobsRoute from './routes/HarvestableJobsRoute';
 import JobsRoute from './routes/JobsRoute';
 import FullJobRoute from './routes/FullJobRoute';
 import RecordsRoute from './routes/RecordsRoute';
+import MikeRoute from './routes/MikeRoute';
+import Tabs from './Tabs';
+import css from './index.css';
+
 
 const HarvesterAdminApp = (props) => {
   const {
@@ -25,14 +28,18 @@ const HarvesterAdminApp = (props) => {
     return <Settings {...props} />;
   }
 
-  // Don't redirect to a page we don't have permission to view: see routes/SwitchRoute.js
+  // Don't redirect to a page we don't have permission to view: see Tabs.js
   const dest = stripes.hasPerm('ui-harvester-admin.harvestables.view') ? 'harvestables' : 'jobs';
 
   return (
-    <Switch>
-      <Redirect exact from={path} to={`${path}/${dest}`} />
-      <NestedRoute path={`${path}`} component={SwitchRoute}>
+    <div className={css.container}>
+      <div className={css.header}>
+        <Tabs />
+      </div>
+      <div className={css.body}>
         <Switch>
+          <Redirect exact from={path} to={`${path}/${dest}`} />
+          <NestedRoute path={`${path}/mike`} exact component={MikeRoute} />
           <NestedRoute path={`${path}/harvestables/create/:type`} exact component={CreateHarvestableRoute} />
           <NestedRoute path={`${path}/harvestables/:recId/logs`} exact component={HarvestableLogRoute} />
           <NestedRoute path={`${path}/harvestables/:recId/jobs`} exact component={HarvestableJobsRoute} />
@@ -45,8 +52,8 @@ const HarvesterAdminApp = (props) => {
           </NestedRoute>
           <NestedRoute path={`${path}/records`} component={RecordsRoute} />
         </Switch>
-      </NestedRoute>
-    </Switch>
+      </div>
+    </div>
   );
 };
 
