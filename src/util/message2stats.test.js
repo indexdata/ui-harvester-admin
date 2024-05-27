c2fa111f5f0a07a2053496802e696362f67d16c1
Mike Taylor <mike@indexdata.com>
2024-05-27 20:19:39 +0100

    Add second test-case (same as the first)

--- src/util/message2stats.test.js
+++ src/util/message2stats.test.js
@@ -11,12 +11,22 @@ const testCases = [
       source: { processed: '0', loaded: '0', deleted: '21', failed: '0' }
     },
   },
+  {
+    name: 'new format',
+    input: '  Instances_processed/loaded/deletions(signals)/failed:__255___0___0(0)___255_ Holdings_records_processed/loaded/deleted/failed:__351___0___0___0_ Items_processed/loaded/deleted/failed:__274___0___0___0_ Source_records_processed/loaded/deleted/failed:__0___0___21___0_',
+    expected: {
+      instances: { processed: '255', loaded: '0', deleted: '0(0)', failed: '255' },
+      holdings: { processed: '351', loaded: '0', deleted: '0', failed: '0' },
+      items: { processed: '274', loaded: '0', deleted: '0', failed: '0', },
+      source: { processed: '0', loaded: '0', deleted: '21', failed: '0' }
+    }
+  },
 ];
 
 for (let i = 0; i < testCases.length; i++) {
   const testCase = testCases[i];
 
-  test('parses old-format stats message', () => {
+  test(`parses ${testCase.name} stats message`, () => {
     const stats = message2stats(testCase.input);
 
     for (const key in testCase.expected) {
