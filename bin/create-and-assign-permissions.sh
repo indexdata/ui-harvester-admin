b4f9d69bffb73ed640ac56a69607f616816e6d8f
Mike Taylor <mike@indexdata.com>
2024-10-22 17:06:53 +0100

    Add necessary perms to script

--- bin/create-and-assign-permissions.sh
+++ bin/create-and-assign-permissions.sh
@@ -1,4 +1,8 @@
-userId="$(OK users -q 'username==diku_admin' -j 'RECORDS[].id')"
+. $HOME/git/folio/other/okclient/ok.sh
+
+OK -u harvester_admin -t harvester -h https://snapshot-dev-okapi.folio-dev.indexdata.com
+
+userId="$(OK users -q 'username==harvester_admin' -j 'RECORDS[].id')"
 userPermsId="$(OK perms/users -q userId="$userId" -j 'RECORDS[].id')"
 
 OK perms/permissions -d '{"permissionName": "mod-settings.global.read.mod-harvester-admin"}'
@@ -6,5 +10,8 @@ OK perms/permissions -d '{"permissionName": "mod-settings.global.write.mod-harve
 
 OK perms/users/"$userPermsId"/permissions -d '{"permissionName": "mod-settings.global.read.mod-harvester-admin"}'
 OK perms/users/"$userPermsId"/permissions -d '{"permissionName": "mod-settings.global.write.mod-harvester-admin"}'
+OK perms/users/"$userPermsId"/permissions -d '{"permissionName": "mod-settings.entries.item.post"}'
+OK perms/users/"$userPermsId"/permissions -d '{"permissionName": "mod-settings.entries.collection.get" }'
+OK perms/users/"$userPermsId"/permissions -d '{"permissionName": "mod-settings.entries.item.put" }'
 
 OK -x
